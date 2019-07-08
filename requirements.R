9e6934b8ea2265f837343151e08e0e67db68acfc
gcaviglia <g.caviglia@quintor.nl>
2019-07-08 22:05:14 +0200

    clean up Dockerfile

--- requirements.R
+++ requirements.R
@@ -1,1 +1,3 @@
 install.packages(c('devtools', 'gam','gsl','rgl','RNeXML','RColorBrewer','BiocManager'),repos="https://lib.ugent.be/CRAN/")
+update.packages(ask=F)
+BiocManager::install(c("scran","MAST","monocle","ComplexHeatmap","clusterExperiment","slingshot"), version = "3.9")
--- requirements_bio.R
+++
@@ -1,2 +0,0 @@
-update.packages(ask=F)
-BiocManager::install(c("scran","MAST","monocle","ComplexHeatmap","clusterExperiment","slingshot"), version = "3.9")
